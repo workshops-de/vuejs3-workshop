e306ce5747d7c3de9a216d227532a15649b15cd5
Antony Konstantinidis <ak@digitalessence.de>
2021-01-07 16:30:51 +0100

    Extend-your-store

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,5 +4,15 @@ export default createStore({
   state: {
     books: []
   },
+  mutations: {
+    UPDATE_BOOKS(state, payload) {
+      state.books = payload.books;
+    }
+  },
+  actions: {
+    SET_BOOKS({ commit }, payload) {
+      commit('UPDATE_BOOKS', payload);
+    }
+  },
   strict: process.env.NODE_ENV !== 'production'
 });
